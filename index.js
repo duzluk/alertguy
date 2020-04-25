222fc81b2002021051fc763502449dc317230021
lincolnaleixo <lincolnmorais@gmail.com>
2020-04-26 00:49:50 +0200

    requests to node-fetch. update eslint

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const request = require('request')
+/* eslint-disable require-jsdoc */
+const fetch = require('node-fetch')
 const applescript = require('applescript')
 const nodemailer = require('nodemailer')
 const os = require('os')
@@ -6,52 +7,40 @@ const os = require('os')
 class Alerts {
 
 	constructor(config) {
-
 		this.config = config
 		this.osType = os.type()
 			.toLowerCase()
-
 	}
 
 	async sendTelegramMessage(message) {
-
 		try {
-
 			console.log('Sending telegram message')
 
 			if (this.config.telegram !== undefined) {
-
 				const { token } = this.config.telegram
 				const { chatId } = this.config.telegram
 				const url = encodeURI(`https://api.telegram.org/bot${token}/sendMessage?chat_id=${chatId}&text=${message}&parse_mode=HTML`)
-				await request.post(url)
+				await fetch(url, { method: 'post' })
+				// await request.post(url) // removing for v14
 				console.log('telegram message sent')
 
 				return true
-
 			}
 
 			console.log('Telegram config not found')
-
 		} catch (error) {
-
 			console.log(`Error on sending telegram message: ${error.stack}`)
-
 		}
 
 		return false
-
 	}
 
 	async sendEmail(emailInfo, emailProvider) {
-
 		// ONLY GMAIL and HOTMAIL SUPPORTED NOW
 		try {
-
 			console.log('Sending email message')
 
 			if (this.config.email !== undefined) {
-
 				const transporter = await nodemailer.createTransport({
 					// host: this.config.email.host,
 					// port: this.config.email.port,
@@ -62,7 +51,6 @@ class Alerts {
 						pass: this.config.email.pass,
 					},
 				})
-
 				const mailOptions = {
 					from: this.config.email.userEmail,
 					to: this.config.email.userEmail,
@@ -71,37 +59,23 @@ class Alerts {
 				}
 
 				await transporter.sendMail(mailOptions, (error, info) => {
-
 					if (error) {
-
 						console.log(error.stack)
-
 					} else {
-
 						console.log(`Email sent: ${info.response}`)
-
 					}
-
 				})
-
 			}
-
 		} catch (error) {
-
 			console.log(`Error on sendEmail: ${error.stack}`)
-
 		}
-
 	}
 
 	sendImessage(guid, message) {
-
 		if (this.getOS() !== 'Mac') {
-
 			console.log('Script must be hosted on a Mac computer for iMessage to work')
 
 			return false
-
 		}
 
 		console.log('Sending iMessage')
@@ -114,29 +88,20 @@ class Alerts {
 		end tell`
 
 		applescript.execString(script, (err, rtn) => {
-
 			if (err) {
-
 				console.log(`Error on sending iMessage message: ${err}`)
-
 			}
 
 			if (Array.isArray(rtn)) {
-
 				for (const songName of rtn) {
-
 					console.log(songName)
-
 				}
-
 			}
-
 		})
-
 	}
 
 	getOS() {
-
+		// TODO retirar e usar de cawer
 		let osName = 'Other'
 
 		if (this.osType === 'darwin') osName = 'Mac'
@@ -144,7 +109,6 @@ class Alerts {
 		else if (this.osType.indexOf('win') > -1) osName = 'Windows'
 
 		return osName
-
 	}
 
 }
